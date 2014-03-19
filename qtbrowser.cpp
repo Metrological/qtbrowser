98f48b2a8fcb151a2864ce4fc93b934cc133711b
albertd <albert@dahan.nl>
2014-03-19 22:22:49 +0100

    qtbrowser: adding license information

--- qtbrowser.cpp
+++ qtbrowser.cpp
@@ -1,3 +1,23 @@
+/**
+ *      qtbrowser
+ *      Copyright (c) 2014 Metrological
+ *      http://www.metrological.com
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with qtbrowser; see the file LICENSE.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ **/
 #include <QtGui>
 
 #if (QT_VERSION < QT_VERSION_CHECK(5, 0, 0))
