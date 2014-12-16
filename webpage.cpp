164066cc6dfc8821de6c667e2a0a274fc6b5b881
Philippe Normand <philn@igalia.com>
2014-12-16 19:40:55 +0100

    WK1: check UA cache before returning overriden value
    The white list of urls/UAs should always keep precedence over
    overriden UserAgent.

--- webpage.cpp
+++ webpage.cpp
@@ -30,10 +30,14 @@ bool WebPage::shouldInterruptJavaScript()
 
 QString WebPage::userAgentForUrl(const QUrl& _url_) const
 {
+    QString url = _url_.url();
+    if (userAgents.contains(url))
+        return userAgents[url];
+
     if (!defaultUserAgent.isEmpty())
         return defaultUserAgent;
 
-    return userAgents.value(_url_.url(), QWebPage::userAgentForUrl(_url_));
+    return QWebPage::userAgentForUrl(_url_);
 }
 
 void WebPage::setDefaultUserAgent(QString _agent_)
