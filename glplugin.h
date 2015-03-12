fe0cd497fec67c15cf687b35b55016831641e44d
Miguel Gomez <magomez@igalia.com>
2015-03-12 15:35:24 +0100

    Added js communication to the plugin.
    When the plugin gets added to the scene, it retrieves its QWebFrame that
    has been stored by webkit as a property. Then it uses addToJavaScriptWindowObject
    to make itself visible to the js environment in that frame.
    
    Also added a signal and a slot to the plugin and updated the html example
    to use them. The example uses a slot to set a number and prints the value
    when a signal is received.

--- glplugin.h
+++ glplugin.h
@@ -4,6 +4,7 @@
 #include <QGraphicsWidget>
 #include <QOpenGLContext>
 #include <QOffscreenSurface>
+#include <QWebFrame>
 
 class QPainter;
 
@@ -15,14 +16,24 @@ public:
     ~GLPlugin();
     void paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget);
 
+public slots:
+    void setNumber(int n);
+
+signals:
+    void numberSet(int n);
+
 protected:
     virtual void keyPressEvent(QKeyEvent* event);
     virtual void keyReleaseEvent(QKeyEvent* event);
     virtual QVariant itemChange(GraphicsItemChange change, const QVariant& value);
 
+private slots:
+    void attachObject();
+
 private:
     QOpenGLContext* m_context;
     QOffscreenSurface* m_surface;
+    QWebFrame* m_frame;
 };
 
 #endif // __PLUGIN__
